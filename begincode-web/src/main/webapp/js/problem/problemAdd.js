b43984e7d9160af0a9c63aa350ecc4945c89e813
ChenHao.Ye <532424981@qq.com>
2016-09-02 17:10:38 +0800

    修改bug

--- begincode-web/src/main/webapp/js/problem/problemAdd.js
+++ begincode-web/src/main/webapp/js/problem/problemAdd.js
@@ -2,6 +2,8 @@
 
 $(document).ready(function(){
     $("#problemSend").click(function(){
+        $("#problemSend").attr("disabled","disabled");
+        $("#problemSend").attr("value","正在提交");
         $('#content').val($('#summernote').summernote('code')); //使summernote里面的内容放到隐藏的content中
         $.ajax({
             data:$("#problemForm").serializeArray(),
@@ -12,11 +14,13 @@ $(document).ready(function(){
                 if(data!=null)
                 {
                     alert(data.msg);
+                    $("#problemSend").removeAttr("disabled");
                 }
 
             },
             error:function(){
                 alert("提交失败");
+                $("#problemSend").removeAttr("disabled");
             }
         });
     });
