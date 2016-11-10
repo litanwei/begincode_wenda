effe4aba157697f2204394c6cd6032bd7a65595d
路人 <Administrator@192.168.0.104>
2016-11-11 00:55:02 +0800

    格式化代码

--- begincode-web/src/main/java/net/begincode/controller/BizUserController.java
+++ begincode-web/src/main/java/net/begincode/controller/BizUserController.java
@@ -1,16 +1,11 @@
 package net.begincode.controller;
 
-import java.util.Collection;
 import java.util.Date;
-import java.util.Enumeration;
-import java.util.HashMap;
-import java.util.Map;
 
 import javax.annotation.Resource;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.stereotype.Controller;
@@ -27,97 +22,134 @@ import net.begincode.core.model.BegincodeUser;
 @Controller
 public class BizUserController {
 	@Resource
-    private UserHandler userHandler;
+	private UserHandler userHandler;
+
 	/**
-	 * 用于显示login中的jsp页面（PS：个人觉得可以建一个view的页面进行各种跳转显示）
-	 * @param folder page下的子文件夹名
-	 * @param pageName 对应的jsp文件名
+	 * 用于显示login中的jsp页面（PS：个人觉得可以建一个viewController的页面进行各种跳转显示）
+	 * 
+	 * @param folder
+	 *            page下的子文件夹名
+	 * @param pageName
+	 *            对应的jsp文件名
 	 * @return
 	 */
-	@RequestMapping(value="/view/{folder}/{pageName}")
-	public String viewPage(@PathVariable(value = "folder") String folder,@PathVariable(value = "pageName") String pageName){
-		return folder+"/"+pageName;
+	@RequestMapping(value = "/view/{folder}/{pageName}")
+	public String viewPage(@PathVariable("folder") String folder, @PathVariable(value = "pageName") String pageName) {
+		return folder + "/" + pageName;
 	}
-	/*
-	 * username登录用户
-	 */
-	@RequestMapping(value="/loginUser")
-	@ResponseBody
-	public Object loginUser(HttpServletRequest request,HttpServletResponse response){
-		String username=request.getParameter("username");
-		String password=request.getParameter("password");
-		BegincodeUser user=userHandler.selectByLoginName(username, password);
-		if(user!=null){//密码账号验证成功
-			CookieOperation.addCookie(response, user);
-			if(userHandler.getLoginUserMap().keySet().contains(username)){
-				userHandler.getLoginUserMap().get(username).invalidate();//清空原有session
-			}
-			userHandler.getLoginUserMap().put(username, request.getSession());//添加新session
-		}else{
-			return false;
-		}
-		return true;
-	}
-	/**
-	 * 检查Cookie中是否存在openid判断登录
-	 * @param request
-	 * @return
-	 */
-	@RequestMapping(value="/checkLogin")
-	@ResponseBody
-	public Object checkLogin(HttpServletRequest request){
-		String openId=getOpenId(request);
-		if(openId==null){
-			return false;
-		}
-		return true;
-	}
+
 	/**
 	 * 用于返回用户的信息 ajax
+	 * 
 	 * @return
 	 */
-	@RequestMapping(value="/userInfo")
+	@RequestMapping(value = "/userInfo",method = RequestMethod.POST)
 	@ResponseBody
-	public Object userInfo(HttpServletRequest request){
-		BegincodeUser user=null;
-		String opend=getOpenId(request);
-		user=userHandler.selectByOpenId(opend);
-		if(user==null){
+	public Object userInfo(HttpServletRequest request) {
+		BegincodeUser user = null;
+		String opend = getOpenId(request);
+		user = userHandler.selectByOpenId(opend);
+		if (user == null) {
 			return null;
 		}
 		return user;
 	}
+
 	/**
-	 * 用户注册
+	 * 登录
+	 * 
+	 * @param loginType
 	 * @param request
 	 * @param response
-	 * @param session
-	 * @return  成功返回true
+	 * @return
 	 */
-	@RequestMapping(value="/registerUser")
+	@RequestMapping(value = "/login/loginUser/{loginType}")
 	@ResponseBody
-	public Object registerUser(HttpServletRequest request,HttpServletResponse response){
-		String username=request.getParameter("username");
-		String nickname=request.getParameter("nickname");
-		String password=request.getParameter("password");
-		String email=request.getParameter("email");
-		String vcode=request.getParameter("vcode").toLowerCase();
-		String checkvcode=request.getSession().getAttribute("imageVcode").toString().toLowerCase();
-		if(!vcode.equals(checkvcode)){
+	public Object login(@PathVariable(value = "loginType") String loginType, HttpServletRequest request,
+			HttpServletResponse response) {
+		if (loginType.equals("client")) {
+			return loginByClient(request, response);
+		}
+		return false;
+	}
+
+	/**
+	 * 注册
+	 * 
+	 * @param registerType
+	 * @param request
+	 * @param response
+	 * @return
+	 */
+	@RequestMapping(value = "/login/registerUser/{registerType}",method = RequestMethod.POST)
+	@ResponseBody
+	public Object register(@PathVariable("registerType") String registerType, HttpServletRequest request,
+			HttpServletResponse response) {
+		if (registerType.equals("client")) { // 客户端注册
+			return registerByClient(request, response);
+		}
+		return false;
+	}
+
+	/**
+	 * QQ注册和登录
+	 * 
+	 * @param response
+	 * @param user
+	 * @return
+	 */
+	@RequestMapping(value = "/login/loginUser/QQ",method = RequestMethod.POST)
+	@ResponseBody
+	public Object loginByQQ(HttpServletResponse response, BegincodeUser user) {
+		user.setUserSourceId(1);
+		user.setDeleteFlag("1");
+		user = userHandler.createUserAndFind(user);
+		CookieOperation.addCookie(response, user);
+		return true;
+	}
+
+	/**
+	 * 检查登录状态 cookie 检查在页面JS检查
+	 * 
+	 * @param request
+	 * @return
+	 */
+	@RequestMapping(value = "/check/checkLogin")
+	@ResponseBody
+	public Object checkLoginStatus(HttpServletRequest request) {
+		if (CookieOperation.getUser(request) != null) {
+			return true;
+		}
+		return false;
+	}
+
+	// -------------------------------------------分割线-------------------------------------
+
+	// 处理client注册
+	public Object registerByClient(HttpServletRequest request, HttpServletResponse response) {
+		String username = request.getParameter("username");
+		String nickname = request.getParameter("nickname");
+		String password = request.getParameter("password");
+		String email = request.getParameter("email");
+		String vcode = request.getParameter("vcode").toLowerCase();
+		String checkvcode = request.getSession().getAttribute("imageVcode").toString().toLowerCase();
+		if (!vcode.equals(checkvcode)) {
 			return false;
 		}
-		if(userHandler.IsExistByRow("login_name", username)){
-			return false;
-		};
-		if(userHandler.IsExistByRow("nickname", nickname)){
-			return false;
-		};
-		if(userHandler.IsExistByRow("email", email)){
+		if (userHandler.IsExistByRow("login_name", username)) {
 			return false;
 		}
-		String open_id="Ro"+RandomStringUtils.randomAlphanumeric(30);
-		String access_token="Ra"+RandomStringUtils.randomAlphanumeric(30);
-		BegincodeUser user=new BegincodeUser();
+		;
+		if (userHandler.IsExistByRow("nickname", nickname)) {
+			return false;
+		}
+		;
+		if (userHandler.IsExistByRow("email", email)) {
+			return false;
+		}
+		String open_id = "Ro" + RandomStringUtils.randomAlphanumeric(30);
+		String access_token = "Ra" + RandomStringUtils.randomAlphanumeric(30);
+		BegincodeUser user = new BegincodeUser();
 		user.setLoginName(username);
 		user.setNickname(nickname);
 		user.setPwd(password);
@@ -126,17 +158,28 @@ public class BizUserController {
 		user.setGag("1");
 		user.setOpenId(open_id);
 		user.setAccessToken(access_token);
-		CookieOperation.addCookie(response, user);//添加cookie信息 用于@AuthPassport验证登录
-		userHandler.getLoginUserMap().put(username, request.getSession()); //添加map 用于map储存session进行异地登录销毁
 		userHandler.addUser(user);
+		CookieOperation.addCookie(response, user);
 		return true;
 	}
 
-	//request获得OpendId
-	public String getOpenId(HttpServletRequest request){
-		Cookie[] cookies=request.getCookies();
-		for(Cookie c:cookies){
-			if(c.getName().equals("openId")){
+	// 处理client登录
+	public Object loginByClient(HttpServletRequest request, HttpServletResponse response) {
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		BegincodeUser user = userHandler.selectByLoginName(username, password);
+		if (user == null) {// 登录失败
+			return false;
+		}
+		CookieOperation.addCookie(response, user);
+		return true;
+	}
+
+	// request获得OpendId
+	public String getOpenId(HttpServletRequest request) {
+		Cookie[] cookies = request.getCookies();
+		for (Cookie c : cookies) {
+			if (c.getName().equals("openId")) {
 				return c.getValue();
 			}
 		}
--- begincode-web/src/main/webapp/js/login/login.js
+++ begincode-web/src/main/webapp/js/login/login.js
@@ -1,7 +1,7 @@
-//显示login弹窗！
+//显示login弹窗
 var layerIndex;
-function view_login(){
-	layerIndex=layer.open({
+function view_login() {
+	layerIndex = layer.open({
 		type : 2,
 		title : '',
 		shadeClose : true,
@@ -9,11 +9,11 @@ function view_login(){
 		area : [ '400px', '400px' ],
 		content : '/user/view/login/login.htm'
 	});
-	layer.close(layerIndex-1);
+	layer.close(layerIndex - 1);
 }
-//显示register弹窗！
-function view_register(){
-	layerIndex=layer.open({
+// 显示register弹窗
+function view_register() {
+	layerIndex = layer.open({
 		type : 2,
 		title : '',
 		shadeClose : true,
@@ -21,206 +21,200 @@ function view_register(){
 		area : [ '680px', '500px' ],
 		content : '/user/view/login/register.htm'
 	});
-	layer.close(layerIndex-1);
+	layer.close(layerIndex - 1);
 	img_change($(".vcode_img"));
 }
-//提交注册表单！
-function form_register_submit(form){
-	if(validate_form(form)){
+// 提交注册表单
+function form_register_submit(form) {
+	if (validate_form(form)) {
 		$.ajax({
-			data:$(form).serialize(),
-			dataType:"json",
-			url:"/user/registerUser.htm",
+			data : $(form).serialize(),
+			type : "post",
+			dataType : "json",
+			url : "/user/login/registerUser/client.htm",
 			success : function(data) {
-				if(data.data==true){
-//					parent.location.reload();//刷新主页面  ########可选
+				if (data.data == true) {
+					// parent.location.reload();//刷新主页面 ########可选
 					parent.layer.close(layerIndex);
-					check_loginStauts();//检查是否登录
-				}else{
+					check_loginStauts();// 检查是否登录
+				} else {
 					img_change($(".vcode_img"));
 				}
 			}
 		});
-	}else{
+	} else {
 		img_change($(".vcode_img"));
 	}
 }
-//可选非法字符
-//else if(!/^[a-z\d\u4E00-\u9FA5]+$/.test(nname.val())){
-//	add_remind(nname,'昵称包含非法字符')
-//	return false;
-//}
-//验证form表单！
-function validate_form(form){
-	var uname=$(form.username);
-	var nname=$(form.nickname);
-	var pwd=$(form.password);
-	var cpwd=$(form.checkpassword);
-	var email=$(form.email);
-	if(uname.val() == ''){
-        add_remind(uname,'请输入用户名');
-        return false;
-    }else if(uname.val().length<6||uname.val().length>15){
-		add_remind(uname,'长度应为6~15位之间')
+// 可选非法字符
+// else if(!/^[a-z\d\u4E00-\u9FA5]+$/.test(nname.val())){
+// add_remind(nname,'昵称包含非法字符')
+// return false;
+// }
+// 验证form表单！
+function validate_form(form) {
+	var uname = $(form.username);
+	var nname = $(form.nickname);
+	var pwd = $(form.password);
+	var cpwd = $(form.checkpassword);
+	var email = $(form.email);
+	if (uname.val() == '') {
+		add_remind(uname, '请输入用户名');
 		return false;
-	}else if(!/^[a-zA-Z0-9_]{6,15}$/.test(uname.val())){
-		add_remind(uname,'包含非法字符')
+	} else if (uname.val().length < 6 || uname.val().length > 15) {
+		add_remind(uname, '长度应为6~15位之间')
 		return false;
-	}else if(nname.val()==''){
-		add_remind(nname,'请输入用户昵称')
+	} else if (!/^[a-zA-Z0-9_]{6,15}$/.test(uname.val())) {
+		add_remind(uname, '包含非法字符')
 		return false;
-	} else if(nname.val().indexOf("nbsp;")>0){
-		add_remind(nname,'昵称包含非法字符')
+	} else if (nname.val() == '') {
+		add_remind(nname, '请输入用户昵称')
 		return false;
-	}else if(pwd.val() == ''){
-        add_remind(pwd,'请输入密码');
-        return false;
-    }else if(cpwd.val() == ''){
-        add_remind(cpwd,'请再次输入密码');
-        return false;
-    }else if(pwd.val().length < 6 || pwd.val().length > 20){
-        add_remind(pwd,'密码应为6~20位之间');
-        return false;
-    }else if(pwd.val() != cpwd.val()){
-        add_remind(pwd,'两次密码不一致');
-        add_remind(cpwd,'两次输入的密码不一样');
-        return false;
-    }else if(email.val() == ''){
-        add_remind(email,'请输入邮箱');
-        return false;
-    }else if(!/\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/.test(email.val())){
-		add_remind(email,'邮箱格式不正确')
+	} else if (nname.val().indexOf("nbsp;") >= 0) {
+		add_remind(nname, '昵称包含非法字符')
+		return false;
+	} else if (pwd.val() == '') {
+		add_remind(pwd, '请输入密码');
+		return false;
+	} else if (cpwd.val() == '') {
+		add_remind(cpwd, '请再次输入密码');
+		return false;
+	} else if (pwd.val().length < 6 || pwd.val().length > 20) {
+		add_remind(pwd, '密码应为6~20位之间');
+		return false;
+	} else if (pwd.val() != cpwd.val()) {
+		add_remind(pwd, '两次密码不一致');
+		add_remind(cpwd, '两次输入的密码不一样');
+		return false;
+	} else if (email.val() == '') {
+		add_remind(email, '请输入邮箱');
+		return false;
+	} else if (!/\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/.test(email.val())) {
+		add_remind(email, '邮箱格式不正确')
 		return false;
 	}
 	return true;
-	}
-//红色提醒字！
-function add_remind(jObject,text){
-	jObject.parent().append('<div class="h5_valid_tip"><p>'+text+'</p></div>');
+}
+// 红色提醒字！
+function add_remind(jObject, text) {
+	jObject.parent().append(
+			'<div class="h5_valid_tip"><p>' + text + '</p></div>');
 	jObject.parent().addClass('h5_valid_wrap');
-	 jObject.on('keyup',function(){
-        jObject.parent().removeClass('h5_valid_wrap');
-        jObject.parent().find('.h5_valid_tip').remove();
-        jObject.off('keyup');
-    });
-	}
-//改变验证图片！
-function img_change(dom){
-	dom.src="/image/vcode.htm?t="+Math.random();  
+	jObject.on('keyup', function() {
+		jObject.parent().removeClass('h5_valid_wrap');
+		jObject.parent().find('.h5_valid_tip').remove();
+		jObject.off('keyup');
+	});
 }
-//登录！
-function login_submit(form) {
-	var uname=$(form.username);
-	var pwd=$(form.password);
-	if(uname.val()==""){
-		 add_remind(uname,'请输入用户名');
-	     return false;
-	}else if(pwd.val()==""){
-		  add_remind(pwd,'请输入密码');
-	       return false;
-	}
-	$.ajax({
-		data:$(form).serialize(),
-		dataType:"json",
-		url:"/user/loginUser.htm",
-		success : function(data) {
-			if(data.data==true){
-//				parent.location.reload();//登录成功刷新主页面  ￥￥￥￥￥￥￥可选可控制
-				parent.layer.close(layerIndex);
-				check_loginStauts(); //登录成功检查一次
-			};
-		}
-	});	
-}
-//检查登录状态 true表示成功登录！
-function check_loginStauts(){
-	$.ajax({
-		dataType:"json",
-		url:"/user/checkLogin.htm",
-		success : function(data) {
-			if(data.data==true){
-				changeLoignDiv(true);
-				return true;
-			}else{
-//				check_loginByQQ();//失败再去用QQCookies判断
-				changeLoignDiv(false);
-				return false;
-			}
-		}
-	});	
+// 改变验证图片！
+function img_change(dom) {
+	dom.src = "/image/vcode.htm?t=" + Math.random();
 }
 
-//退出登录!
-function exitLogin(){
+// 检查登录状态 true表示成功登录！
+function check_loginStauts() {
 	$.ajax({
-		type:"post",
-		url:"/user/loginClean.htm",
-		success : function(data){
-			check_loginStauts();
-			QC.Login.signOut();
+		dataType : "json",
+		url : "/user/check/checkLogin.htm",
+		success : function(data) {
+			if (data.data == true) {
+				changeLoignDiv(true);
+			}
 		}
 	});
 }
-//修改div登录显示!
-function changeLoignDiv(boolen){
-	var _html;
-	if(boolen==false){
-	  _html=$("#failLoginDiv").html();
-	}else{
-	  _html=$("#successLoginDiv").html();
+
+// 登录！
+function login_submit(form) {
+	var uname = $(form.username);
+	var pwd = $(form.password);
+	if (uname.val() == "") {
+		add_remind(uname, '请输入用户名');
+		return false;
+	} else if (pwd.val() == "") {
+		add_remind(pwd, '请输入密码');
+		return false;
 	}
 	$.ajax({
-		dataType:"json",
-		url:"/user/userInfo.htm",
-		success : function(data){
-			var user=data.data;
-			disposeDiv(_html,user);
+		data : $(form).serialize(),
+		dataType : "json",
+		url : "/user/login/loginUser/client.htm",
+		success : function(data) {
+			if (data.data == true) {
+				parent.layer.close(layerIndex);
+				check_loginStauts(); // 登录成功检查一次
+			}
+			;
 		}
 	});
 }
-//替换div中的参数!
-function disposeDiv(_html,array){
-	var reg = new RegExp(/\[(.*?)\]/,'igm');
-	_html=_html.replace(reg,function(node, key){
+
+// 退出登录!
+function exitLogin() {
+	$.ajax({
+		type : "post",
+		url : "/user/loginClean.htm",
+		success : function(data) {
+			changeLoignDiv(false);
+		}
+	});
+}
+// 修改div登录显示!
+function changeLoignDiv(boolen) {
+	var _html;
+	if (boolen == false) {
+		_html = $("#failLoginDiv").html();
+	} else {
+		_html = $("#successLoginDiv").html();
+	}
+	$.ajax({
+		type : "post",
+		dataType : "json",
+		url : "/user/userInfo.htm",
+		success : function(data) {
+			var user = data.data;
+			disposeDiv(_html, user);
+		}
+	});
+}
+// 替换div中的参数!
+function disposeDiv(_html, array) {
+	var reg = new RegExp(/\[(.*?)\]/, 'igm');
+	_html = _html.replace(reg, function(node, key) {
 		return array[key];
 	});
 	$("#simpleLogin").html(_html);
 }
 
-//QQ登录页面打开!
-function login_submitByQQ(){
-	url="https://graph.qq.com/oauth/show?which=Login&display=pc&client_id=101230380&response_type=token&scope=get_user_info,upload_pic,get_user_cbinfo&redirect_uri=http%3A%2F%2Fwww.begincode.net%2Flogin.html";
-//	layer.iframeSrc(layerIndex, url); //不能回传
+// QQ登录页面打开
+function login_submitByQQ() {
 	QC.Login.showPopup();
 	layer.close(layerIndex);
-//	window.open(url); 可选窗口打开
 }
 // QQ登录成功会回调
-var paras={};
-QC.api("get_user_info", paras)  
-.success(function(s){//成功回调  
-	reqd=s.data;
-	QC.Login.getMe(function(openId, accessToken){
-		if(check_loginStauts()==false){
-			return;
-		}
-		regUser(reqd.nickname, reqd.figureurl, reqd.gender, reqd.province, reqd.city, reqd.year, openId, accessToken);
+var paras = {};
+QC.api("get_user_info", paras).success(
+		function(s) {// 成功回调
+			reqd = s.data;
+			QC.Login.getMe(function(openId, accessToken) {
+				regUser(reqd.nickname, reqd.figureurl, reqd.gender,
+						reqd.province, reqd.city, reqd.year, openId,
+						accessToken);
+			});
 		});
-}) ; 
-//QQ注册或登录
-function regUser(nickName, figureurl, gender, province, city, year, openId, accessToken) {
-    jQuery.ajax({
-        type: "POST",
-        url:"/user/login.htm",
-        data: "nickname=" + nickName + "&pic=" + figureurl + "&sex=" + gender + "&openId=" + openId + "&accessToken=" + accessToken,
-        dataType: "json",
-        success: function (data) {
-        },
-        error: function(){
-        	QC.Login.signOut();
-        	location.reload();
-        }
-    });
+// QQ注册或登录
+function regUser(nickName, figureurl, gender, province, city, year, openId,
+		accessToken) {
+	jQuery.ajax({
+		type : "POST",
+		url : "/user/login/loginUser/QQ.htm",
+		data : "nickname=" + nickName + "&pic=" + figureurl + "&sex=" + gender
+				+ "&openId=" + openId + "&accessToken=" + accessToken,
+		dataType : "json",
+		success : function(data) {
+			QC.Login.signOut();
+			location.reload();
+		}
+	});
 
 }
-
